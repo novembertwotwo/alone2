366e1f3a7b9d0f89a59cc4114aa5bad6fac7da0b
novembertwotwo <korea621214@naver.com>
2025-02-12 15:50:55 +0900

    업로드 결과의 반환 처리

--- src/main/java/book/alone/controller/UpDownController.java
+++ src/main/java/book/alone/controller/UpDownController.java
@@ -1,6 +1,7 @@
 package book.alone.controller;
 
 import book.alone.dto.UploadFileDTO;
+import book.alone.dto.UploadResultDTO;
 import lombok.extern.slf4j.Slf4j;
 import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +15,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -24,25 +27,29 @@ public class UpDownController {
     private String uploadPath;
 
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String upload(UploadFileDTO uploadFileDTO) {
+    public List<UploadResultDTO> upload(UploadFileDTO uploadFileDTO) {
         log.info("{}", uploadFileDTO);
         if (uploadFileDTO.getFiles() != null) {
+            final List<UploadResultDTO> list = new ArrayList<>();
             uploadFileDTO.getFiles().forEach(multipartFile -> {
                 String originalFilename = multipartFile.getOriginalFilename();
                 log.info("{}", originalFilename);
                 String uuid = UUID.randomUUID().toString();
+                boolean image = false;
                 Path savePath = Paths.get(uploadPath, uuid + "_" + originalFilename);
                 try {
                     multipartFile.transferTo(savePath);
                     if (Files.probeContentType(savePath).startsWith("image")) {
+                        image = true;
                         File thumbFile = new File(uploadPath, "s_" + uuid + "_" + originalFilename);
                         Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200, 200);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
+                list.add(UploadResultDTO.withAll().img(image).uuid(uuid).fileName(originalFilename).build());
             });
+            return list;
         }
         return null;
     }
